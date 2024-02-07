b7067913b54bd36caf1139eba3b5ce88674ccb73
Guy_R <guynkama@gmail.com>
2024-02-07 11:13:36 +0100

    ajout simple jwt token auth

--- evenements/gestionEvenements/views.py
+++ evenements/gestionEvenements/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Evenement
 from .serializers import EvenementSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class EvenementViewSet(viewsets.ModelViewSet):
     queryset = Evenement.objects.all()
     serializer_class = EvenementSerializer
+    permission_classes = [IsAuthenticated]
 
