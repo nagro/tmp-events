df5e671cf50db31b237bf1c01390085b6273c73e
Guy_R <guynkama@gmail.com>
2024-02-08 01:22:39 +0100

    handle cors

--- evenements/evenements/settings.py
+++ evenements/evenements/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'gestionEvenements',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'evenements.urls'
@@ -68,6 +70,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 WSGI_APPLICATION = 'evenements.wsgi.application'
